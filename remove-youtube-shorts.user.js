8912a771711005b27f56226e827d284dc1d33b18
Meg923a <scottcheung31@gmail.com>
2024-06-05 22:46:27 +0800

    20240605

--- remove-youtube-shorts.user.js
+++ remove-youtube-shorts.user.js
@@ -2,6 +2,7 @@
 // @name         Remove YouTube Shorts
 // @namespace    https://github.com/Meg923a
 // @version      0.1
+// @description  A simple script to remove Youtube shorts.
 // @author       Meg923a
 // @match        *://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
