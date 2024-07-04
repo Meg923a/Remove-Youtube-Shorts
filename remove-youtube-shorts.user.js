8a64ccaf781c1daa81c862d95782420d50742d85
Meg923a <scottcheung31@gmail.com>
2024-07-04 21:45:53 +0800

    20240704

--- remove-youtube-shorts.user.js
+++ remove-youtube-shorts.user.js
@@ -7,18 +7,27 @@
 // @match        *://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // ==/UserScript==
+
 (() => {
     const removeShorts = () => {
-        const shortsTab = document.querySelectorAll(".style-scope:nth-child(1) > #items > .style-scope:nth-child(2) > #endpoint, .ytd-mini-guide-renderer:nth-child(2) > #endpoint");
-        const mainPageShorts = document.querySelectorAll("[is-shorts]");
-        const watchPageShorts = document.querySelectorAll("ytd-reel-shelf-renderer");
-        const searchPageShorts = document.querySelectorAll("ytd-video-renderer:has(div > ytd-thumbnail > a[href*='shorts'])");
+        const shortsTab = document.querySelectorAll(
+            ".style-scope:nth-child(1) > #items > .style-scope:nth-child(2) > #endpoint, .ytd-mini-guide-renderer:nth-child(2) > #endpoint"
+        );
+        const mainPageShorts = document.querySelectorAll(
+            "[is-shorts]"
+        );
+        const watchPageShorts = document.querySelectorAll(
+            "ytd-reel-shelf-renderer"
+        );
+        const searchPageShorts = document.querySelectorAll(
+            "ytd-video-renderer:has(div > ytd-thumbnail > a[href*='shorts'])"
+        );
 
         const shortsElements = [
             ...shortsTab,
             ...mainPageShorts,
             ...watchPageShorts,
-            ...searchPageShorts
+            ...searchPageShorts,
         ];
 
         shortsElements.forEach((element) => {
